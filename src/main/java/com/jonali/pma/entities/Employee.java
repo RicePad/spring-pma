7e3f120b6a9c02b66ce4c0d14f2e9225d505f499
JONATHAN <hello@jonali.me>
2020-12-21 14:36:48 -0500

    smooth many-to-many relationship

--- src/main/java/com/jonali/pma/entities/Employee.java
+++ src/main/java/com/jonali/pma/entities/Employee.java
@@ -2,14 +2,26 @@ package com.jonali.pma.entities;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Employee {
 	
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long employeeId;
@@ -18,9 +30,12 @@ public class Employee {
 	private String lastName;
 	private String email;
 	
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST},
+			   fetch = FetchType.LAZY)
+	@JoinTable(name="project_employee",
+	joinColumns=@JoinColumn(name="employee_id"),
+	inverseJoinColumns= @JoinColumn(name="project_id"))
 	private List<Project> projects;
-	
-	
 	
 	public Employee() {
 		
@@ -32,6 +47,7 @@ public class Employee {
 		this.lastName = lastName;
 		this.email = email;
 	}
+	
 	public long getEmployeeId() {
 		return employeeId;
 	}
