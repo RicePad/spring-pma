cf0eae4f15acbe8357e0cfa4edaf01d9fed97971
JONATHAN <hello@jonali.me>
2021-01-03 11:56:40 -0500

    Http rquest test passed sucessfully

--- src/test/java/com/jonali/pma/controllers/HttpRequestTest.java
+++ src/test/java/com/jonali/pma/controllers/HttpRequestTest.java
@@ -3,10 +3,16 @@ package com.jonali.pma.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 	
 	@LocalServerPort
@@ -19,6 +25,5 @@ public class HttpRequestTest {
 	public void homePageReturnsVersionNumberCorrectly_thenSuccess() {
 		String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
 		assertEquals(renderedHtml.contains("3.3.3"), true);
-
 	}
 }
